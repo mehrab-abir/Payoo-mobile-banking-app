2e8c73a51930fb138f9ad7bd473302c57baebfe0
Mehrab-Abir <mjabir@myseneca.ca>
2025-08-28 16:21:38 -0400

    UI Design and login functionality

---
+++ home.js
@@ -0,0 +1,22 @@
+//login section
+document.getElementById('login-btn').addEventListener('click',function(event){
+    event.preventDefault();
+
+    const savedMobileNumber = "12345678901"
+    const savedPinNumber = "1234";
+
+    const userMobileNumber = document.getElementById('user-mobile-number').value;
+    const userPin = document.getElementById('user-pin').value;
+
+    // console.log(userMobileNumber, userPin);
+
+    if(userMobileNumber == savedMobileNumber && userPin == savedPinNumber){
+        window.location.href = 'home.html';
+    }
+    else{
+        alert(`Invalid Credentials. Use ${savedMobileNumber} as mobile number and ${savedPinNumber} as 4 digit PIN`);
+    }
+})
+
+
+//add money section
